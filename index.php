c74c00047efd3912bbd4f15253e301b1e5aefa08
Daniel Torres <2130072@upv.edu.mx>
2024-03-25 07:57:25 -0600

    correciones & nav_bar

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php
-echo 'Apartado para mostrar navegación';
+<html lang="es">
+<head>
+    <title>Menú principal</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+</head>
+<body>
+<div class="container mt-5 mb-5">
+    <h2>Menú Principal</h2>
+    <?php include 'nav_bar.html'; ?>
+</div>
+</body>
+</html>
--- listado_carreras.php
+++ listado_carreras.php
@@ -3,18 +3,20 @@
     include 'conexion.php';
 
     //hacemos la consulta para obtener los registros de la tabla carreras
-    $sql = "SELECT * FROM carrera";
+    $sql = "SELECT * FROM carreras";
     $result = $conn->query($sql);
 ?>
 
 <html lang="es">
     <head>
-        <title>Listado de Carreras</title>
+        <title>Carreras</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 
     </head>
     <body>
+    <?php include "nav_bar.html"; ?>
         <div class="container mt-5">
+            <a href="." class="btn btn-secondary mb-2">Regresar</a>
             <h2>Listado de Carreras</h2>
             <!--<a href="alta_carrera.php" class="btn btn-primary">Agregar Carrera</a>-->
             <table class="table">
@@ -30,13 +32,13 @@
                         while($row = $result->fetch_assoc()){
                     ?>
                     <tr>
-                        <td><?php echo $row['id_carrera']; ?></td>
+                        <td><?php echo $row['id']; ?></td>
                         <td><?php echo $row['nombre']; ?></td>
                         <td>
                             <!-- el get se representa con el signo de "?" y el nombre de la variable-->
-                            <a href="editar_carrera.php?id_carrera=<?php echo $row['id_carrera']; ?>" class="btn btn-primary">Editar</a>
+                            <a href="editar_carrera.php?id_carrera=<?php echo $row['id']; ?>" class="btn btn-primary">Editar</a>
                             <form class="d-inline" action="crud.php" method="POST">
-                                <input type="hidden" name="id_carrera" value="<?php echo $row['id_carrera']; ?>">
+                                <input type="hidden" name="id_carrera" value="<?php echo $row['id']; ?>">
                                 <button type="submit" class="btn btn-danger" name="eliminar_carrera">Eliminar</button>
                             </form>
                         </td>
@@ -47,7 +49,7 @@
                 </tbody>
             </table>
             <a href="alta_carrera.php" class="btn btn-success">Agregar Carrera</a>
-            <a href="exportar_carreras.php" class="btn btn-info">Exportar a XLS</a>
+<!--            <a href="exportar_carreras.php" class="btn btn-info">Exportar a XLS</a>-->
             <br>
             <br>
         </div>
